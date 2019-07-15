2f6756578fb577aef819ea0c83aab8640864b46a
Kevin Hoffman <kehoffman3@gmail.com>
2019-07-15 16:03:33 -0400

    Update list fields. Catch exceptions on error

--- vue_plugins/admin.py
+++ vue_plugins/admin.py
@@ -9,7 +9,7 @@ from vue_plugins.models import VuePlugin
 
 class VuePluginAdminExtra(ModelAdmin):
     actions = ['update_plugin_info']
-    list_display = ['repo_url', 'name', 'tag_list']
+    list_display = ['name', 'repo_url', 'tag_list', 'last_release_date', 'num_commits_recently', 'num_contributors', 'num_stars']
 
     def get_queryset(self, request):
         return super().get_queryset(request).prefetch_related('tags')
@@ -43,11 +43,17 @@ class VuePluginAdminExtra(ModelAdmin):
         return super(VuePluginAdminExtra, self).get_fieldsets(request, obj)
 
     def update_plugin_info(self, request, queryset):
+        error_count = 0
         for plugin in list(queryset):
-            plugin.update_info_from_github()
+            try:
+                plugin.update_info_from_github()
+            except Exception as e:
+                # Log exception and continue
+                error_count += 1
+                print('plugin {} failed for reason: {}'.format(plugin.repo_url, str(e)))
 
         plugin_count = len(queryset)
-        self.message_user(request, "%s plugins(s) were updated!" % plugin_count)
+        self.message_user(request, "%s plugins(s) were updated!" % (plugin_count - error_count))
 
 
 admin.site.register(VuePlugin, VuePluginAdminExtra)
