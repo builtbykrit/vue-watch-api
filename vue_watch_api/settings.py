e7b746c44d2ebecbebc3a3ad1b221af8394dbdc3
Kevin Hoffman <kehoffman3@gmail.com>
2019-08-23 15:47:12 -0400

    Update db settings

--- vue_watch_api/settings.py
+++ vue_watch_api/settings.py
@@ -88,7 +88,10 @@ WSGI_APPLICATION = 'vue_watch_api.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'vue_watch',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', None),
+        'HOST': os.environ.get('DB_HOST')
     }
 }
 
